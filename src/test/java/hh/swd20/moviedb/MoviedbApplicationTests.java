4294cde44335b6d4b6086f7e8ff25d383362046d
Eviix <daniel.ristikari@gmail.com>
2022-04-14 16:30:57 +0300

    First commit

---
+++ src/test/java/hh/swd20/moviedb/MoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
