21885bd3a7837fee7377e2fcea9042256fb45a4d
Eviix <daniel.ristikari@gmail.com>
2022-04-18 01:28:39 +0300

    added better styling when adding & editing movie

--- src/main/java/hh/swd20/moviedb/domain/MovieRepository.java
+++ src/main/java/hh/swd20/moviedb/domain/MovieRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
-    List<Movie> findByLastName(String title);
+    List<Movie> findByTitle(String title);
     
 }
